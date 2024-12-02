ca2d230359fd7d234e155833d9ee95e22beae05a
divyanjali2003 <divyanjalidileep78@gmail.com>
2024-12-02 12:55:39 +0530

    Add files via upload

--- python/ass.py
+++ python/ass.py
@@ -25,9 +25,47 @@ list1=["red","rose","black","white","blue"]
 # list1[4]="brown"
 # print(list1)
 
-n=int(input("Enter a number"))
-factorial=1
-for i in range(1,n+1):
-    factorial=factorial*i
-print("Factorial of the given number is",factorial)
+# n=int(input("Enter a number"))
+# factorial=1
+# for i in range(1,n+1):
+#     factorial=factorial*i
+# print("Factorial of the given number is",factorial)
 
+# list1=[1,2,3,4,5,6]
+# list2=sum(list1)
+# print(list2)
+
+# list2=("apple","banana","orange")
+# list4=sum(list2)
+# print(list4)
+
+# numbers=(2,3,4,5,6,7)
+# sum=sum(numbers)
+# print(sum)
+
+# dic1={1,2,3,4,5,6,7,8,9}
+# dic2=sum(dic1)
+# print(dic2)
+
+# a1= [[1, 1, 1, 1], 
+#      [2, 2, 2, 2], 
+#      [3, 3, 3, 3], 
+#      [4, 4, 4, 4]]
+# result=[[row[i] for row in a1] for i in range(len(a1[0]))]
+# for row in result:
+#     print("join([str(element) for element in row])")     
+
+
+a = [[1,2],[2,3],[7,9]]
+
+a1= [[0, 0, 0], [0, 0, 0]]
+
+for i in range (len (a)):
+
+              for j in range (len (a[0])):
+
+                             a1 [j][i] = a [i][j]
+
+for r in a1:
+
+              print (r)
