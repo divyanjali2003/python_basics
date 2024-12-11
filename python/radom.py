53b5b85a4b979edab3fcfd09759f4e0ea45d30c8
divyanjali2003 <divyanjalidileep78@gmail.com>
2024-12-11 18:00:57 +0530

    Add files via upload

---
+++ python/radom.py
@@ -0,0 +1,55 @@
+# import math
+# x=math.sqrt(8)
+# print(x)
+
+# import math
+# x=math.sqrt(6)
+# print(x)
+
+# import math
+# x=math.ceil(1.4)
+# y=math.floor(1.4)
+# print(x)
+# print(y)
+
+# import math 
+# x=math.pi
+# print(x)
+
+# import datetime
+# x=datetime.datetime.now()
+# print(x)
+
+# import datetime
+# x=datetime.datetime.now()
+# print(x.year)
+# print(x.strftime("%Y"))
+
+# import datetime
+# x=datetime.datetime(2003,1,19)
+# print(x)
+
+# import random
+# random.seed(20)
+# print(random.random())
+
+# import random
+# print(random.randrange(3,9))
+
+# import random
+# print(random.randint(2,3))
+
+# x=min(5,10,25)
+# y=max(5,10,25)
+# print(x)
+# print(y)
+
+# import calendar
+# yy=2003
+# mm=1
+# print(calendar.month(yy,mm))
+
+import calendar
+yy=int(input("enter year:"))
+mm=int(input("enter month:"))
+print(calendar.month(yy,mm))
