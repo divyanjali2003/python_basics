d2e9474805b6b0904fce383131b35264c4e3ef5d
divyanjali2003 <divyanjalidileep78@gmail.com>
2024-12-13 09:39:58 +0530

    Add files via upload

---
+++ python/file.py
@@ -0,0 +1,84 @@
+# f=open("test.txt","x")
+
+# f=open("test.txt","w")
+# f.write("my name is divya")
+# f.close()
+
+# f=open("test.txt","r")
+# print(f.read())
+
+# f=open("test.txt","a")
+# f.write("my age is 21")
+# f.close()
+
+# f=open("test.txt","r")
+# print(f.read(5))
+
+# f=open("test.txt","r")
+# print(f.readline())
+
+# f=open("test.txt","r")
+# print(f.readlines())
+
+# f=open("test.txt","r")
+# print(f.readlines(3))
+
+# f=open("test.txt","r")
+# print(f.read())
+
+# a=open("test.txt","w")
+# a.write("i have cat")
+# a.close()
+
+# f=open("test.txt","w")
+# f.writelines(["i have dog","\n","dog will bark"])
+# f.close()
+
+# a=open("test.txt","w")
+# a.writelines(["my name is divya","\n","im studying"])
+# a.close()
+
+# s=open("test.txt","w")
+# s.write("i have pets")
+# s.close()
+
+# d=open("test.txt","r")
+# print(d.read())
+
+# v=open("test.txt","r")
+# print(v.readline(11))
+
+# f = open("test.txt", "r")
+# for x in f:
+#     print(x)
+
+# d=open("add.txt","x")
+
+# d=open("add.txt","w")
+# d.write("i have dog")
+# d.close()
+
+# d=open("add.txt","r")
+# print(d.read())
+
+# d=open("adqd.txt","w")
+# d.write("my name is Divyanjali")
+# d.close()
+
+# d=open("add.txt","r")
+# print(d.read(3))
+
+# d=open("test.txt","r")
+# print(d.readlines(4))
+
+# d=open("aded.txt","r")
+# print(d.readlines(3))
+
+# d=open("aded.txt","w")
+# d.writelines(["i have 2 pen","\n","but "])
+# d.close()
+
+# d=open("aded.txt", "r")
+# print(d.readline())
+# d.close()
+
