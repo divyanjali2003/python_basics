1ca42a8c395ca49e2d1b77065bcac10934ad70c7
divyanjali2003 <divyanjalidileep78@gmail.com>
2024-12-14 09:44:13 +0530

    Add files via upload

---
+++ python/csvv.py
@@ -0,0 +1,26 @@
+# import csv
+# with open("test.csv","w",newline='')as f:
+#     writer=csv.writer(f)
+#     writer.writerow(["name","age","place"])
+#     writer.writerow(["Divya","21","Alappuzha"])
+#     writer.writerow(["jiji","24","kollam"])
+
+# import csv
+# f= open("test.csv","r")
+# reader=csv.reader(f)
+# for r in reader:
+#     print(r)
+
+# import csv
+# with open("test.csv","w",newline='') as s:
+#     company=csv.writer(s)
+#     company.writerrow(["name","age","year"])
+#     company.writerow(["anu","34","1954"])
+#     company.writerow(["vinu","37","1956"])
+#     company.writerow(["suku","32","1965"])
+
+# import csv
+# s=open("test.csv","r")
+# reader=csv.reader(s)
+# for r in reader:
+#     print(r)
